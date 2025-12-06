121cba4eb60e04210bde08b4993b77054193a69b
s-mariamfarag-blip <s-mariam.farag@zewailcity.edu.eg>
2025-12-06 22:00:43 +0200

    add my work

---
+++ Library Mangment System/controllers/borrow_controller.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, render_template, request, redirect, session
+from models.book import Book
+from models.borrow_record import BorrowRecord
+
+borrow_bp = Blueprint("borrow", __name__, url_prefix="/borrow")
+
+@borrow_bp.route("/", methods=["GET", "POST"])
+def borrow_book():
+    if "userid" not in session:
+        return redirect("/user/login")
+
+    if request.method == "POST":
+        bookID = request.form["bookID"]
+        studentID = session["userid"]
+
+        # Create borrow record
+        BorrowRecord.create(studentID, bookID)
+
+        # Update book status to borrowed
+        Book.update_status(bookID, "borrowed")
+
+        return redirect("/user/dashboard")
+
+    books = Book.get_all()
+    return render_template("borrow/borrow.html", books=books)
